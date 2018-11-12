fed20985631e372410ca2b0eb8aee86eadd83b5f
Adam <adam@iii.com>
2018-11-13 01:20:27 +0800

    initialization

---
+++ trigonometry/src/calc/TrigonometryCalc.java
@@ -0,0 +1,21 @@
+package calc;
+
+import javax.swing.JFrame;
+
+public class TrigonometryCalc {
+
+	public static void main(String[] args) {
+		
+		//版面配置
+		JFrame frame=new JFrame("三角函數計算機 V1.0");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(260,250);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		//結束配置 顯示視窗
+		frame.setVisible(true);
+
+	}
+
+}
