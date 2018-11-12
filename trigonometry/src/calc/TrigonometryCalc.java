39050d460bb7a97d9baa05005623846f2ec2a484
Adam <adam@iii.com>
2018-11-13 01:25:57 +0800

    panel setup

--- trigonometry/src/calc/TrigonometryCalc.java
+++ trigonometry/src/calc/TrigonometryCalc.java
@@ -4,6 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TrigonometryCalc {
@@ -20,6 +21,14 @@ public class TrigonometryCalc {
 		JPanel panel=new JPanel(new GridBagLayout());
 		frame.getContentPane().add(panel);
 		GridBagConstraints gbc=new GridBagConstraints();
+		
+		JLabel title=new JLabel("請於下方輸入數值");
+		title.setHorizontalAlignment(JLabel.CENTER);
+		gbc.fill=GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth=2;
+		gbc.gridx=0;
+		gbc.gridy=0;
+		panel.add(title,gbc);
 		
 		//結束配置 顯示視窗
 		frame.setVisible(true);
